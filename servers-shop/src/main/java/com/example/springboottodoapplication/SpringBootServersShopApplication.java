4fd2f299a9a00c68a2dd72c9b9059b6ffe19bd5b
Justas Urbanavicius <justas.urbanavicius@mif.stud.vu.lt>
2023-11-21 20:04:35 +0200

    changed psql to h2 for easier testing

--- servers-shop/src/main/java/com/example/springboottodoapplication/SpringBootServersShopApplication.java
+++ servers-shop/src/main/java/com/example/springboottodoapplication/SpringBootServersShopApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootToDoApplication {
+public class SpringBootServersShopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootToDoApplication.class, args);
+		SpringApplication.run(SpringBootServersShopApplication.class, args);
 	}
 
 }
